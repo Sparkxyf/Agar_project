c58fd10f0de9aba19bd274bce5770b4dca464f51
Garinyu <38396863+Garinyu@users.noreply.github.com>
2018-06-22 16:25:01 +0800

    res

---
+++ Resources/SpaceBG1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SpaceBG1" tilewidth="128" tileheight="128" tilecount="120" columns="15">
+ <image source="../Space/SpaceBG1.jpg" width="1920" height="1080"/>
+</tileset>
